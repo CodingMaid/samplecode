d4b00553a1838af83b7d12795992cb9d1540f56c
CodingMaid <94186032+CodingMaid@users.noreply.github.com>
2021-11-12 23:31:18 +0800

    Create samplecode.js

---
+++ samplecode.js
@@ -0,0 +1,12 @@
+/*   Sample code	*/
+/*   Please read over instructions first!	*/
+const world = "world"
+const my_important_message = 
+"Hello " + world    
+
+if (world != 
+'world') {
+console.log("This cannot happen")
+}
+
+console.log(my_important_message)
